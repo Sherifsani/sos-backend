020f8d98d309e7637bec310178c4ceb54b427f9a
Anuolu-2020 <Anuolu1645@gmail.com>
2025-04-02 21:28:27 +0100

    feat: hello controller implemented

---
+++ src/main/java/com/example/sosbackend/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.sosbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.sosbackend.dto.HelloResponse;
+import com.example.sosbackend.service.HelloService;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+  private final HelloService helloService;
+
+  public HelloController(HelloService helloService) {
+    this.helloService = helloService;
+  }
+
+  @GetMapping
+  public HelloResponse sayHello() {
+    String message = this.helloService.getGreeting();
+    return new HelloResponse(message);
+  }
+}
