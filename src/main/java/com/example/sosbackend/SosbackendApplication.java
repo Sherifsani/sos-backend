68d3b42f0c1637aedbb918dec06dba79fd3327ad
Anuolu-2020 <Anuolu1645@gmail.com>
2025-04-02 21:29:14 +0100

    chore: generated by spring boot initializer

---
+++ src/main/java/com/example/sosbackend/SosbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosbackendApplication.class, args);
+	}
+
+}
