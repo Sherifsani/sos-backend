dd33cb03e61a18cca2c5738962841771b575a71a
Anuolu-2020 <Anuolu1645@gmail.com>
2025-04-09 11:47:38 +0100

    feat: custom query for fetching nearest emergency service within a coordinate implemented

--- src/main/java/com/example/sosbackend/repository/EmergencyServiceRepository.java
+++ src/main/java/com/example/sosbackend/repository/EmergencyServiceRepository.java
@@ -1,9 +1,18 @@
 package com.example.sosbackend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.sosbackend.model.EmergencyServicesModel;
 
 public interface EmergencyServiceRepository extends JpaRepository<EmergencyServicesModel, Long> {
 
+  @Query(value = "SELECT * FROM emergency_services WHERE ST_DWithin(coordinates, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326), :radius)", nativeQuery = true)
+  List<EmergencyServicesModel> findNearbyEmergencyServices(
+      @Param("longitude") double longitude,
+      @Param("latitude") double latitude,
+      @Param("radius") double radius);
 }
